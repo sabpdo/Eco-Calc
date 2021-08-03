d17a82fb71bee29898dd51a1f9ffcefd1a98919f
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-08-03 10:21:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// var menuNav = $('.menu-nav');
+
+// window.onresize = window.onload = function() {
+// if ($(window).width() < 770) {
+//    menuNav.html(`
+//     <a id="drop" href="">
+//     <ul>
+//       <li>⬇️</li>
+//     </ul>
+//   <div class="dropdown-content">
+//     <a href="index.html">🏠</a>
+//     <a href="carbon.html">💨</a>
+//     <a href="waterp.html">👩‍🦱💧</a>
+//     <a href="waterb.html">🏢💧</a>
+//     <a href="info.html">📂ℹ️</a>
+//     <a href="about.html">👥👥</a>
+//   </div>
+//   </a>
+//   `);
+
+  event.preventDefault();
+}
+}
